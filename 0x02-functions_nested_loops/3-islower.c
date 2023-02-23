6db22c45ad6a96d183302c473d30bab3960b75f6
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-23 15:15:02 +0000

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Check main
+ *
+ * Return: 1 if is lowercase or 0 if not
+ */
+int _islower(int c)
+{
+  int c
+    if (c>='a" && c<= "z")
+        return (1);
+    else
+	      return (0);
+}
