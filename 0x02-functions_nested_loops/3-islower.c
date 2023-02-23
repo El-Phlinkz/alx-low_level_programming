473bd89eb8597a4515974b5fd3f2561aef9435ae
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-23 15:20:58 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,10 @@
  */
 int _islower(int c)
 {
-  int c
-    if (c>='a" && c<= "z")
-        return (1);
-    else
-	      return (0);
+	int c;
+
+	if (c >= "a" && c <= "z")
+		return (1);
+	else
+		return (0);
 }
