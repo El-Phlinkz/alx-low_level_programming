37cd388aac7c2c7a9cbb2e8c21c742f76f09e205
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-23 07:02:09 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 {
-		putchar(letter);
+		putchar(letter,"\n");
 }
 	return (0);
 }
