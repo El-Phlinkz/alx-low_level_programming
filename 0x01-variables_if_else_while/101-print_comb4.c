18a24200db6f881ed8153d7afc69b4d335590693
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-23 10:49:17 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n, m, o;
 
 	for (n = '0'; n < '9'; n++)
 	{
-		for (n = m + 1; m <= '9'; m++)
+		for (m = n + 1; m <= '9'; m++)
 		{
-for (o = m +1; o <= 9; o++)
-{
-			if ((m != n) != o)
+			for (o = m + 1; o <= '9'; o++)
 			{
-				putchar(n);
-				putchar(m);
-putchar(o);
+				if ((m != n) != o)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(o);
 
-				if (n == '8' && m == '9')
-					continue;
+					if (n == '7' && m == '8')
+						continue;
 
-				putchar(',');
-				putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
