001e820a2d78ce556fc64de1de8c3a31b1024b13
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-23 15:43:30 +0000

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,8 @@ void print_alphabet(void)
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-{
-    _putchar(letter);
-}
-	putchar('\n');
+		_putchar(letter);
+
+	_putchar('\n');
 	return (0);
 }
