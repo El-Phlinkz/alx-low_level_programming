89879bc455604a63ad14dbab0bb8325a55eac795
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-24 16:40:42 +0000

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,12 @@
  * Return: Always 0
  */
 void print_most_numbers(void)
-{
-        int n = '0';
+	int n = '0';
 
-        while (n <= '9')
-        {
-                if (n != '2' && n != '4')
-                        _putchar(n);
-                        n++;
-        }
-        _putchar('\n');
+	for (; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+			_putchar(n);
+	}
+	_putchar('\n');
 }
