dd6609c44e406a8d60e755f0c6dc955429c1ae53
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-24 10:55:40 +0000

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	int n;
-
 	if (n % 10 < 0)
 		_putchar((n % 10) * -1);
 	else if (n % 10 > 0)
-    _putchar((n % 10);
+    _putchar(n % 10);
 	else
 	return (n % 10);
 }
