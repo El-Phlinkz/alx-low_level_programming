1d99489865ebd1d587f2fa2f9ac1c766dc57ad9f
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-27 09:31:15 +0000

    Create 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a diagonal line on the terminal.
+ * followed by new line.
+ * @n: An input integer
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int m = 0;
+
+	if (n > 0)
+	{
+		for (; m < n; m++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
