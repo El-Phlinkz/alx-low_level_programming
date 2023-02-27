c27ba661c2ed1daadba21cd884295fc5d20aa649
El-Phlinkz <123077471+El-Phlinkz@users.noreply.github.com>
2023-02-27 11:12:52 +0000

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int m = 0;
+		int o = 0;
+
+	if (n > 0)
+	{
+		for (; m < n; m++)
+		{
+			for (o = 0; o < m; o++)
+				putchar(' ');
+		putchar(92);
+		putchar('\n');
+		}
+	}
+	else
+		putchar('\n');
