b1ada186683a337157f18b571216edf366c04eff
Laud Ankrah <laudankrah@gmail.com>
2023-06-18 10:38:47 -0700

    size is not

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char o;
+	int p;
+	long int q;
+	long long int r;
+	float s;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(o));
+	printf("Size of a int: %d byte(s)\n", sizeof(p));
+	printf("Size of a long int: %d byte(s)\n", sizeof(q));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(r));
+	printf("Size of a float: %d byte(s)\n", sizeof(s));
+
+	return (0);
+}
