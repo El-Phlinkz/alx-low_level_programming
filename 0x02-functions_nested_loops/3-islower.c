b72d368644026e73d8d1a7b2ae8e304e54155335
Laud Ankrah <laudankrah@gmail.com>
2023-06-21 02:10:25 -0700

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - check next line
+ * checks for lowercase character.
+ * Return 0
+ */
+int _islower(int c)
+{
+	char ALP;
+	int i = 0;
+
+	for (ALP = 'a' ; ALP <= 'z' ; ALP++)
+	{
+		if (ALP == 'c')
+			i = 1;
+	}
+	return(i);
+}
