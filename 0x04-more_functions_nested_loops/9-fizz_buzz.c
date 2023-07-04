a5978b1d4d6c5d07e5debfe21260d11fcf0e3f1a
Laud Ankrah <laudankrah@gmail.com>
2023-07-04 07:55:28 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-  int i = 1;
-  
-  for (; i < 100 ; i++)
+	int i = 1;
+	
+	for (; i < 100 ; i++)
 	{
-    if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
