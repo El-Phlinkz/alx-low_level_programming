93e349a7770beb06b203b1a3200f406439e76fc2
Laud Ankrah <laudankrah@gmail.com>
2023-07-04 04:32:38 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - check next line
+ * draws a diagonal line
+ * @n: an input integer
+ * Return: Always 0
+ */
+void print_line(int n)
+{
+	int s;
+	int d;
+
+	if (n > 0)
+	{
+		for (s = 0; s < n; s++)
+		{
+			for (d = 0; d < s; d++)
+				_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
