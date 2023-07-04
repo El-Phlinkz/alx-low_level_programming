3a2e69af38a769be2216f745ba038c0be8b19da0
Laud Ankrah <laudankrah@gmail.com>
2023-07-04 03:25:59 -0700

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isdigit - check next line
+ * checks for a digit
+ * @c: an input digit
+ * Return: 1 if c is a digit  and 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	int r = 0;
+	char digit;
+
+	for (digit  = '0'; digit <= 'Z'; digit++)
+	{
+		if (c == digit)
+		{
+			r = 1;
+			break;
+		}
+	}
+
+	return (r);
+}
